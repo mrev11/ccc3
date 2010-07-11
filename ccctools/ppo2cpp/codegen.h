f328ddb156475e196ff53612e5eeb25fc8dc14e0
Vermes Matyas <vermes@comfirm.hu>
2010-07-11 15:01:10 +0200

    hiányzó copyright-ok

--- ccctools/ppo2cpp/codegen.h
+++ ccctools/ppo2cpp/codegen.h
@@ -1,4 +1,23 @@
 
+/*
+ *  CCC - The Clipper to C++ Compiler
+ *  Copyright (C) 2005 ComFirm BT.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 extern int codegen_prg_header_lfunction(parsenode *p,void *v);
 extern int codegen_header_lnewline0_namespace_lusing_lstatdefin(parsenode *p,void *v);
 extern int codegen_namespace(parsenode *p,void *v);

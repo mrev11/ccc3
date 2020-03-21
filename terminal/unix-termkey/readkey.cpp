cd3d19c4ab5123a0505c41a03e86e037f04ed230
vermes <vermes@comfirm.hu>
2020-03-21 15:32:38 +0100

    2020-03-21 terminal/unix-termkey: A libtermkey Linux/NetBSD-en Shift-F1-et Key=<F13>-nak adja FreeBSD-n Shift-F1-et Key=<F1>-nek mod=SHIFT modositassal adja (hasonlo elteres minden Fn gombra es modositora) ezt a kulonbseget FreeBSD-n ki kell egyenliteni.

--- terminal/unix-termkey/readkey.cpp
+++ terminal/unix-termkey/readkey.cpp
@@ -1,4 +1,22 @@
 
+/*
+ *  CCC - The Clipper to C++ Compiler
+ *  Copyright (C) 2005 ComFirm BT.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 //#include <stdio.h>
 //#include <unistd.h>
@@ -58,6 +76,21 @@ int readkey()
             if( 1<=n && n<=12 )
             {
                 inkeycode=-(200+n);
+
+                //freebsd
+                int mod=key.modifiers;
+                if( mod&4 )
+                {
+                    inkeycode-=200; //ctrl
+                }
+                else if( mod&2 )
+                {
+                    inkeycode-=300; //alt
+                }
+                else if( mod&1 )
+                {
+                    inkeycode-=100; //shift
+                }
             }
             else if( 13<=n && n<=24 )
             {

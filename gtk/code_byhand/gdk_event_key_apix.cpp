62516f17853e1a2ed1380d31a50f503e150cd71c
vermes <vermes@comfirm.hu>
2024-04-12 11:34:36 +0200

    2024-04-12 gtk interfesz bovitve: gdk.event_key.get_keyval();

---
+++ gtk/code_byhand/gdk_event_key_apix.cpp
@@ -0,0 +1,41 @@
+
+/*
+ *  CCC - The Clipper to C++ Compiler
+ *  Copyright (C) 2005 ComFirm BT.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <gdk/gdk.h>
+#include <cccapi.h>
+#include <cccgtk.h>
+
+
+namespace _nsp_gdk {
+namespace _nsp_event_key {
+
+//---------------------------------------------------------------------------
+void _clp_get_keyval(int argno) //kiegészítés (miért nincs ilyen?)
+{
+    CCC_PROLOG("gdk.event_key.get_keyval",1);
+    gpointer event=_parp(1);
+    _retni( ((GdkEventKey*)event)->keyval );
+    CCC_EPILOG();
+}
+
+//---------------------------------------------------------------------------
+} //event_key
+} //gdk
+
--- gtk/code_byhand/m.bat
+++ gtk/code_byhand/m.bat
@@ -14,6 +14,7 @@ call bapp_w32_.bat @gtk.bld  -lccc3_gtk
 copy include\gtk.ch     %CCCDIR%\usr\include\gtk.ch
 copy include\gdk.ch     %CCCDIR%\usr\include\gdk.ch
 copy include\cccgtk.h   %CCCDIR%\usr\include\cccgtk.h
+copy include\gdkkey.ch  %CCCDIR%\usr\include\gdkkey.ch
 
 copy obj%CCCBIN%\ccc3_gtk.lib %CCCDIR%\usr\lib\%CCCBIN%\ccc3_gtk.lib
 
